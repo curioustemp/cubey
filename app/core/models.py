530c983885b48a1d1a5eadc9d6d299df9d411a0a
awinn <adambwinn+temp@gmail.com>
2016-07-24 19:38:25 -0700

    Initial commit: use flask-user 0.6.8 to get working. Also make basic prototype.

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,8 @@ from flask_wtf import Form
 from wtforms import StringField, SubmitField, validators
 from app import db
 
+from datetime import datetime
+
 
 # Define the User data model. Make sure to add the flask_user.UserMixin !!
 class User(db.Model, UserMixin):
@@ -15,12 +17,13 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
 
     # User authentication information (required for Flask-User)
+    username = db.Column(db.String(10), nullable=False, unique=True)
     email = db.Column(db.Unicode(255), nullable=False, server_default=u'', unique=True)
     confirmed_at = db.Column(db.DateTime())
     password = db.Column(db.String(255), nullable=False, server_default='')
     reset_password_token = db.Column(db.String(100), nullable=False, server_default='')
     active = db.Column(db.Boolean(), nullable=False, server_default='0')
-
+    banned = db.Column(db.Boolean(), nullable=False, server_default='0')
     # User information
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='0')
     first_name = db.Column(db.Unicode(50), nullable=False, server_default=u'')
@@ -50,16 +53,52 @@ class UsersRoles(db.Model):
 # Define the User registration form
 # It augments the Flask-User RegisterForm with additional fields
 class MyRegisterForm(RegisterForm):
-    first_name = StringField('First name', validators=[
-        validators.DataRequired('First name is required')])
-    last_name = StringField('Last name', validators=[
-        validators.DataRequired('Last name is required')])
+    submit = SubmitField('Submit')
 
 
 # Define the User profile form
 class UserProfileForm(Form):
-    first_name = StringField('First name', validators=[
-        validators.DataRequired('First name is required')])
-    last_name = StringField('Last name', validators=[
-        validators.DataRequired('Last name is required')])
+    email = StringField('email', validators=[
+        validators.DataRequired('Must supply email')])
     submit = SubmitField('Save')
+
+class Upvote(db.Model):
+    __tablename__ = 'upvotes'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    plugin_id = db.Column(db.Integer(), db.ForeignKey('plugins.id', ondelete='CASCADE'))
+
+    plugin = db.relationship("Plugin", back_populates="upvotes")
+
+    def __init__(self, user_id, plugin_id):
+        self.user_id = user_id
+        self.plugin_id = plugin_id
+
+def _get_date():
+    return datetime.utcnow() 
+
+class Plugin(db.Model):
+    __tablename__ = 'plugins'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    name = db.Column(db.String(50), nullable=False, server_default=u'')
+    description = db.Column(db.String(300), nullable=False, server_default=u'')
+    repo_url = db.Column(db.String(300), nullable=False, server_default=u'')
+    readme = db.Column(db.String(5000), nullable=False, server_default=u'')
+    created_at = db.Column(db.DateTime, default=_get_date)
+    updated_at = db.Column(db.DateTime, onupdate=_get_date)
+    image_url = db.Column(db.String(300))
+
+    upvotes = db.relationship('Upvote', order_by=Upvote.id, back_populates="plugin", lazy='dynamic')
+
+    def __init__(self, name, description, user_id, repo_url, image_url, created_at=None):
+        self.name = name
+        self.description = description
+        self.user_id = user_id
+        self.repo_url = repo_url
+        self.image_url = image_url
+        if created_at is None:
+            created_at = datetime.utcnow()
+        self.created_at = created_at
--- app/core/views.py
+++ app/core/views.py
@@ -7,7 +7,7 @@ from flask import redirect, render_template, render_template_string, Blueprint
 from flask import request, url_for
 from flask_user import current_user, login_required, roles_accepted
 from app import app, db
-from app.core.models import UserProfileForm
+from app.core.models import UserProfileForm, Plugin, Upvote
 
 core_blueprint = Blueprint('core', __name__, url_prefix='/')
 
@@ -15,8 +15,36 @@ core_blueprint = Blueprint('core', __name__, url_prefix='/')
 # The Home page is accessible to anyone
 @core_blueprint.route('')
 def home_page():
-    return render_template('core/home_page.html')
+    sub_query = db.session.query(Upvote.plugin_id, db.func.count('*').\
+        label('upvote_count')).\
+        group_by(Upvote.plugin_id).subquery()
 
+    has_liked_query = db.session.query(Upvote, db.func.exists(Upvote.user_id == current_user.id).\
+            label('has_upvoted')).subquery()
+
+    plugins = db.session.query(Plugin, sub_query.c.upvote_count).\
+        outerjoin(sub_query, Plugin.id==sub_query.c.plugin_id).order_by(Plugin.id)
+        # outerjoin(has_liked_query, current_user.id==has_liked_query.c.user_id).order_by(Plugin.id)
+
+    return render_template('core/home_page.html', plugins=plugins)
+
+@core_blueprint.route('my-plugins', methods=['GET', 'POST'])
+def plugin_form():
+    if request.method == 'POST':
+        form = request.form
+        plugin = Plugin(name=form['name'],
+                        description=form['description'],
+                        user_id=current_user.id,
+                        repo_url=form['repo_url'],
+                        image_url=form['image_url'])
+        plugin.upvotes = [
+            Upvote(user_id=current_user.id, plugin_id=plugin.id)
+        ]
+        db.session.add(plugin)
+        db.session.commit()
+
+    plugins = Plugin.query.filter_by(user_id=current_user.id)
+    return render_template('core/plugin_manager.html', plugins=plugins)
 
 # The User page is accessible to authenticated users (users that have logged in)
 @core_blueprint.route('user')
